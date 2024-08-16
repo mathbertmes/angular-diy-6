6593a9473afa4ee082b353393a4f46a05d37dd7a
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-15 18:37:12 -0700

    ROUTES

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { SecretComponent } from './secret/secret.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "register", component: RegisterComponent},
+  {path: "secret", component: SecretComponent}
+];
