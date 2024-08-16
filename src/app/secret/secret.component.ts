22a6ff0b84cae7fccf8b04e9c5edde4b7f3dd70c
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-15 20:59:57 -0700

    secret page

--- src/app/secret/secret.component.ts
+++ src/app/secret/secret.component.ts
@@ -1,12 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-secret',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './secret.component.html',
   styleUrl: './secret.component.css'
 })
 export class SecretComponent {
-
+  password: string | undefined = undefined;
+  ngOnInit() {
+    const userData = localStorage.getItem('user')
+    if(userData){
+      this.password = JSON.parse(userData).password
+    }
+  }
 }
